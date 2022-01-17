930c7dbaaa806f5576520c2b5eb3ffab5ccc50c1
Andrea Davide <hobiwelten@gmail.com>
2022-01-17 14:44:01 +0100

    navbar almost done

--- forum-dci/forumdci/src/components/Navbar.jsx
+++ forum-dci/forumdci/src/components/Navbar.jsx
@@ -1,61 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div class="navbar bg-primary mb-2 shadow-lg  text-neutral-content rounded-box">
-      <div class="px-2 mx-2 navbar-start">
-        <a
-          to="/"
-          class="text-lg btn font-bold ml-5 border-none hover:bg-transparent bg-transparent text-4xl"
-        >
-          AVAZ
-        </a>
-      </div>
-      <div class="hidden px-2 mx-2 navbar-center lg:flex">
-        <div class="flex items-stretch">
-          <a class="btn btn-ghost btn-sm rounded-btn">Home</a>
-          <a class="btn btn-ghost btn-sm rounded-btn">Questions</a>
-          <a class="btn btn-ghost btn-sm rounded-btn">Tags</a>
-          <a class="btn btn-ghost btn-sm rounded-btn">Community</a>
-          <a class="btn btn-ghost btn-sm rounded-btn">About</a>
+  const [isShown, setIsShown] = useState(false);
 
-          <button class="btn bg-tertiary glass text-primary hover:bg-gray-500 hover:text-white btn-sm ">
-            ASK SOMETHING
+  return (
+    <div>
+      <div class="navbar bg-primary  shadow-lg  text-neutral-content md:rounded-box">
+        {/* LOGO AREA */}
+
+        <div class="px-2 mx-2 navbar-start">
+          <button
+            onMouseEnter={(e) => {
+              setIsShown(true);
+            }}
+            onClick={(e) => {
+              setIsShown(false);
+            }}
+            className="lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-8 w-8"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
           </button>
+          <a
+            to="/"
+            class="text-lg btn font-bold lg:ml-5 border-none hover:bg-transparent bg-transparent text-4xl"
+          >
+            AVAZ
+          </a>
+        </div>
+
+        {/* MENU AREA */}
+        <div class="hidden px-2 mx-2 lg:flex">
+          <ul class="flex">
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Questions</a>
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Tags</a>
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Community</a>
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">About</a>
+
+            <button class="btn bg-secondary glass mx-8 text-primary hover:bg-gray-600 hover:text-white btn-sm ">
+              ASK SOMETHING
+            </button>
+          </ul>
+        </div>
+
+        {/* LOG IN AREA */}
+        <div class="navbar-end flex items-center content-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-7 w-7"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <a class="btn btn-ghost btn-sm mr-2 pt-1 text-lg md:text-sm rounded-btn">
+            Log In
+          </a>
         </div>
       </div>
-      <div class="navbar-end">
-        <button class="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-6 h-6 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            ></path>
-          </svg>
-        </button>
-        <button class="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-6 h-6 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </button>
-      </div>
+
+      {isShown && (
+        <div className="bg-primary px-6 mb-2 md:hidden shadow-lg">
+          <ul className="flex-col">
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Questions</a>
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Tags</a>
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Community</a>
+            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">About</a>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
