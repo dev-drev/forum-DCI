18f2355694c682fa7967ce0713034f246cec58d3
Andrea Davide <hobiwelten@gmail.com>
2022-01-17 10:51:51 +0100

    project init

---
+++ forum-dci/forumdci/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1 className="p-8">Hello World</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
