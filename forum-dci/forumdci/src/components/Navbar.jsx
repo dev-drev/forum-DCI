9fac76188f73bd94a02d9ebd1bb8e82514cf98d5
Andrea Davide <hobiwelten@gmail.com>
2022-01-17 15:23:18 +0100

    navbar updated

--- forum-dci/forumdci/src/components/Navbar.jsx
+++ forum-dci/forumdci/src/components/Navbar.jsx
@@ -59,14 +59,16 @@ const Navbar = () => {
         <div class="navbar-end flex items-center content-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-7 w-7"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
           >
             <path
-              fill-rule="evenodd"
-              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-              clip-rule="evenodd"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
             />
           </svg>
           <a class="btn btn-ghost btn-sm mr-2 pt-1 text-lg md:text-sm rounded-btn">
@@ -76,12 +78,41 @@ const Navbar = () => {
       </div>
 
       {isShown && (
-        <div className="bg-primary px-6 mb-2 md:hidden shadow-lg">
+        <div className="bg-primary px-6 mb-2 sm:hidden shadow-lg">
           <ul className="flex-col">
-            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Questions</a>
-            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Tags</a>
-            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">Community</a>
-            <a class="btn btn-ghost btn-sm mx-4 rounded-btn">About</a>
+            <a href="">
+              {" "}
+              <li class="mx-2 p-4 flex content-center items-center hover:glass rounded-btn">
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>{" "}
+                Questions{" "}
+              </li>
+            </a>
+            <a href="">
+              {" "}
+              <li class="mx-2 p-4 hover:glass pointer rounded-btn"> Tags </li>
+            </a>
+            <a href="">
+              {" "}
+              <li class="mx-2 p-4  hover:glass rounded-btn"> Community </li>
+            </a>
+            <a href="">
+              {" "}
+              <li class="mx-2 p-4 hover:glass rounded-btn"> About </li>
+            </a>
           </ul>
         </div>
       )}
