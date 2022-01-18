aec86cb01f7c2fa7415a96e77920141201c1492f
87vivi <viviana.castaldo@gmx.de>
2022-01-19 00:09:11 +0100

    admin page

--- forum-dci/forumdci/src/App.js
+++ forum-dci/forumdci/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
+import Avatar from "./components/adminPage/Admin";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Avatar />
     </div>
   );
 }
