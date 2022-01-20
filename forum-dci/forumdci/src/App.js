a65eb1adee62c51e2a22f9adf84aa993d01ac4b1
Alcione <alcionefribeiro@gmail.com>
2022-01-20 14:40:45 +0100

    Add AskSomethingPage

--- forum-dci/forumdci/src/App.js
+++ forum-dci/forumdci/src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 import Login from "./components/Login/Login.jsx";
 import Navbar from "./components/Navbar.jsx";
+import SubNavbar from "./components/QuestionsPage/SubNavbar";
 import Avatar from "./components/adminPage/Admin";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import QuestionsPage from "./components/Questions Page/QuestionsPage.jsx";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import QuestionsPage from "./components/QuestionsPage/QuestionsPage.jsx";
 import SinglePostPage from "./components/QuestionPage/SinglePostPage.jsx";
+import AskSomethingPage from "./components/QuestionPage/AskSomethingPage.jsx";
+import PopularPosts from "./components/QuestionsPage/PopularPosts.jsx";
+
 
 function App() {
     return (
         <Router>
             <Navbar/>
+            <SubNavbar/>
             <Routes>
-                <Route path="/questions" element={<QuestionsPage/>}/>
-                <Route path="/answer" element={<SinglePostPage/>}/>
+                {/*<Route path="/questions" element={<QuestionsPage/>}/>*/}
+                {/*<Route path="/answer" element={<SinglePostPage/>}/>*/}
+                <Route path="/ask" element={<AskSomethingPage/>}/>
+                <Route path="/popular" element={<PopularPosts/>}/>
             </Routes>
         </Router>
     );
 
 }
+
 export default App;
