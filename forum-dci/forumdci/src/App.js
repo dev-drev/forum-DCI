81dc640bc6e98ceabad4b0eaff552492c68273b8
Andrea Davide <hobiwelten@gmail.com>
2022-01-21 13:34:26 +0100

    layout almost finished- components added

--- forum-dci/forumdci/src/App.js
+++ forum-dci/forumdci/src/App.js
@@ -8,9 +8,11 @@ import QuestionsPage from "./components/QuestionsPage/QuestionsPage.jsx";
 import SinglePostPage from "./components/QuestionPage/SinglePostPage.jsx";
 import Footer from "./components/Footer.jsx";
 import TagsBody from "./components/TagsPage/TagsBody.jsx";
+import Community from "./components/Community/Community.jsx";
 
 import TextEditor from "./components/QuestionPage/TextEditor";
 import AskSomethingPage from "./components/QuestionPage/AskSomethingPage";
+import Login from "./components/Login/Login.jsx";
 
 function App() {
   console.log("hello");
@@ -19,17 +21,20 @@ function App() {
       <Router>
         <Navbar />
 
-        <SubNavbar />
         <div>
           <Routes>
+            <Route path="/" exact>
+              {/* <SubNavbar />
+              <Home /> */}
+            </Route>
+            <Route path="/login" element={<Login />} />
             <Route path="/questions" element={<QuestionsPage />}></Route>
             <Route path="/tags" element={<TagsBody />} />
             <Route path="/answer" element={<SinglePostPage />} />
             <Route path="/ask" element={<AskSomethingPage />} />
             <Route path="/answer" element={<TextEditor />} />
             <Route path="/post" element={<SinglePostPage />} />
-            {/* <Route path="/community" element={<SinglePostPage />} />
-            <Route path="/post" element={<SinglePostPage />} /> */}
+            <Route path="/community" element={<Community />} />
           </Routes>
         </div>
       </Router>
