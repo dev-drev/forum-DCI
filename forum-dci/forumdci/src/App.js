c40fa24d7deb96fb6cd6c83cb6ab1fbd5fe7c74d
Andrea Davide <hobiwelten@gmail.com>
2022-01-25 09:29:46 +0100

    app page edited

--- forum-dci/forumdci/src/App.js
+++ forum-dci/forumdci/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar.jsx";
-import AdminPage from "./components/adminPage/AdminPage.jsx";
+import AdminPage from "./components/AdminPage/AdminPage.jsx";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import QuestionsPage from "./components/QuestionsPage/QuestionsPage.jsx";
 import SinglePostPage from "./components/QuestionPage/SinglePostPage.jsx";
-import AboutPage from "./components/aboutPage/AboutPage.jsx";
-
+import AboutPage from "./components/AboutPage/AboutPage.jsx";
+import TagsBody from "./components/TagsPage/TagsBody.jsx";
+import Community from "./components/Community/Community.jsx";
+import Ask from "./components/QuestionPage/AskSomethingPage";
+import Login from "./components/Login/Login";
 function App() {
   console.log("hello");
   return (
@@ -17,6 +20,10 @@ function App() {
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/questions" element={<QuestionsPage />} />
         <Route path="/answer" element={<SinglePostPage />} />
+        <Route path="/tags" element={<TagsBody />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/ask" element={<Ask />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
