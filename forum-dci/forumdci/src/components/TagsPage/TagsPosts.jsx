c0876402af668e410841667709d072776b96de94
Alcione <alcionefribeiro@gmail.com>
2022-02-09 15:33:11 +0100

    Task: searching question by tag

--- forum-dci/forumdci/src/components/TagsPage/TagsPosts.jsx
+++ forum-dci/forumdci/src/components/TagsPage/TagsPosts.jsx
@@ -1,48 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import hashtag from "../../assets/hashtag.png";
 import TagsCard from "./TagsCard";
 import TagsSearch from "./TagsSearch";
 
 function TagsPosts() {
-  return (
-    <div>
-      {" "}
-      <div className="flex justify-between">
-        <div className="flex-col mb-4 content-center items-center">
-          <div className="flex content-center items-center ">
-            <h3 className="text-5xl text-shadow text-white font-semibold">
-              TAGS
-            </h3>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-10 ml-2 w-10"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-              />
-            </svg>
-          </div>
-          <p className="pt-2 text-secondary">
-            Please select 2-3 tags in order to find your answer faster
-          </p>
+    const [questions, setQuestions] = useState([]);
+
+
+    return (
+        <div>
+            {" "}
+            <div className="flex justify-between">
+                <div className="flex-col mb-4 content-center items-center">
+                    <div className="flex content-center items-center ">
+                        <h3 className="text-5xl text-shadow text-white font-semibold">
+                            TAGS
+                        </h3>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-10 ml-2 w-10"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="white"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                            />
+                        </svg>
+                    </div>
+                    <p className="pt-2 text-secondary">
+                        Please select 2-3 tags in order to find your answer faster
+                    </p>
+                </div>
+                <img src={hashtag} className="w-24 mr-16  h-24" alt=""/>
+            </div>
+            <TagsSearch questions={questions} setQuestions={setQuestions}/>
+            <div className=" flex flex-wrap justify-between pt-4  w-12/12 sm:w-11/12">
+                {questions.map((question, key)=>
+                    (
+                        <TagsCard data={question} key={key}/>
+                    )
+                )}
+
+            </div>
         </div>
-        <img src={hashtag} className="w-24 mr-16  h-24" alt="" />
-      </div>
-      <TagsSearch />
-      <div className=" flex flex-wrap justify-between pt-4  w-12/12 sm:w-11/12">
-        <TagsCard /> <TagsCard /> <TagsCard /> <TagsCard /> <TagsCard />{" "}
-        <TagsCard /> <TagsCard /> <TagsCard /> <TagsCard /> <TagsCard />{" "}
-        <TagsCard /> <TagsCard /> <TagsCard /> <TagsCard /> <TagsCard />{" "}
-        <TagsCard />
-      </div>
-    </div>
-  );
+    );
 }
 
 export default TagsPosts;
--- forum-dci/forumdci/src/server/controllers/questionsController.js
+++ forum-dci/forumdci/src/server/controllers/questionsController.js
@@ -1,78 +1,103 @@
 const Question = require("../models/Question");
-const { validationResult } = require("express-validator");
+const {validationResult} = require("express-validator");
 
 /*add a new user*/
 async function addQuestion(req, res, next) {
-  console.log("You have a question!");
+    console.log("You have a question!");
 
-  /*handle the error*/
-  try {
-    const err = validationResult(req);
-    if (!err.isEmpty()) {
-      return res.status(400).send(err);
+    /*handle the error*/
+    try {
+        const err = validationResult(req);
+        if (!err.isEmpty()) {
+            return res.status(400).send(err);
+        }
+        const {title, language, tags, question, likes} = req.body;
+
+        const response = await Question.create({
+            title,
+            language,
+            question,
+            tags,
+            likes,
+            date: new Date(),
+        });
+        res.status(200).send(response);
+    } catch (err) {
+        console.log(err);
+        next(err);
     }
-    const { title, language, tags, question, likes } = req.body;
-
-    const response = await Question.create({
-      title,
-      language,
-      question,
-      tags,
-      likes,
-      date: new Date(),
-    });
-    res.status(200).send(response);
-  } catch (err) {
-    console.log(err);
-    next(err);
-  }
 }
 
 async function getQuestions(req, res, next) {
-  try {
-    const questions = await Question.find();
-    res.status(200).send(questions);
-  } catch (e) {
-    next(e);
-  }
+    try {
+        const questions = await Question.find();
+        res.status(200).send(questions);
+    } catch (e) {
+        next(e);
+    }
 }
 
 async function getSingleQuestion(req, res, next) {
-  const { searchQuestion } = req.params;
+    const {searchQuestion} = req.params;
 
-  try {
-    var questionsReqExp = new RegExp(searchQuestion, "gi");
-    const questions = await Question.find({
-      title: questionsReqExp,
-    });
-    console.log(questions);
-    res.status(200).send(questions);
-  } catch (e) {
-    next(e);
-  }
+    try {
+        var questionsReqExp = new RegExp(searchQuestion, "gi");
+        const questions = await Question.find({
+            title: questionsReqExp,
+        });
+        console.log(questions);
+        res.status(200).send(questions);
+    } catch (e) {
+        next(e);
+    }
 }
 
 async function getQuestionById(req, res, next) {
-  const { id } = req.params;
+    const {id} = req.params;
 
-  try {
-    const questions = await Question.findById(id);
-    console.log(questions);
-    res.status(200).send(questions);
-  } catch (e) {
-    next(e);
-  }
+    try {
+        const questions = await Question.findById(id);
+        console.log(questions);
+        res.status(200).send(questions);
+    } catch (e) {
+        next(e);
+    }
 }
 
+
+async function getQuestionByTag(req, res, next) {
+    const {tag} = req.params;
+    let query = {tags: tag};
+
+    if (tag === "all") {
+        try {
+            const questions = await Question.find({}).sort({date: 'desc'}).limit(10);
+            console.log(questions);
+            res.status(200).send(questions);
+        } catch (e) {
+            next(e);
+        }
+    } else {
+        try {
+            const questions = await Question.find(query);
+            console.log(questions);
+            res.status(200).send(questions);
+        } catch (e) {
+            next(e);
+        }
+    }
+}
+
+
 const deleteQuestion = async (req, res, next) => {
-  const deleteQuestion = await Question.findByIdAndDelete(req.params.id);
-  if (!deleteQuestion) {
-    res
-      .status(404)
-      .send({ message: "Unable to delete question, please check " });
-  } else {
-    res.status(200).send({ message: "Question deleted successfully" });
-  }
+    const deleteQuestion = await Question.findByIdAndDelete(req.params.id);
+    if (!deleteQuestion) {
+        res
+            .status(404)
+            .send({message: "Unable to delete question, please check "});
+    } else {
+        res.status(200).send({message: "Question deleted successfully"});
+    }
 };
 
 // async function deleteQuestion(req, res, next) {
@@ -91,10 +116,11 @@ const deleteQuestion = async (req, res, next) => {
 // }
 
 module.exports = {
-  addQuestion,
-  getQuestions,
-  getQuestionById,
-  getSingleQuestion,
-  deleteQuestion,
-  // deleteQuestion,
+    addQuestion,
+    getQuestions,
+    getQuestionById,
+    getSingleQuestion,
+    deleteQuestion,
+    // deleteQuestion,
+    getQuestionByTag
 };
